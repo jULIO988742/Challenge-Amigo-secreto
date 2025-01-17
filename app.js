309c85d99ddd30c98b1e7f2e01ed95962658ac66
julio01 <bautistaujulio@gmail.com>
2025-01-17 16:46:54 -0600

    Funcion agregar amigo

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-// Crear una varable quue alamcene la lista de amigos ARRAY
-let amigos = [];
+let nombreIngresado = document.getElementById("amigo"); //traendo la cajeta de ingreso del nombre
+
+function agregarAmigo(){ //agrego el la funcionalidad del click agregar 
+    let listaAmigos = nombreIngresado.value; //Variable que almacena la lista de amigos
+    console.log(listaAmigos)
+    return;
+
+}
+agregarAmigo();
+
